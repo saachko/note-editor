74feae4cdd76cb0e47d4ee1081de2074fb349b2b
Anastasiya Sachko <naztya12323@gmail.com>
2022-11-22 17:01:44 +0300

    feat: add function which gets data from the dataBase

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import { collection, getDocs } from '@firebase/firestore';
+import React, { useEffect, useState } from 'react';
 
 import Form from 'components/Form';
 import MainPageImage from 'components/SvgElements/MainPageImage';
 
+import dataBase from '../firebase';
+import Note from '../utils/interfaces';
+
 function MainPage() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  const notesCollection = collection(dataBase, 'notes');
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const data = await getDocs(notesCollection);
+      const notesFromDataBase: Note[] = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setNotes(notesFromDataBase);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <main>
       <section className="notes-creation">
---
+++ src/utils/interfaces.ts
@@ -0,0 +1,8 @@
+interface Note {
+  id: string;
+  title?: string;
+  text?: string;
+  tags?: string[];
+}
+
+export default Note;
