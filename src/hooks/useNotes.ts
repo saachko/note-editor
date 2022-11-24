8c59f162dbd659b9a4fb517ed9f1298eb3bb7d53
Anastasiya Sachko <naztya12323@gmail.com>
2022-11-24 04:08:46 +0300

    refactor: separate logic into a custom hook

---
+++ src/hooks/useNotes.ts
@@ -0,0 +1,82 @@
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from '@firebase/firestore';
+import { useState } from 'react';
+import { v4 } from 'uuid';
+
+import defaultNote from 'utils/constants';
+import Note from 'utils/interfaces';
+
+import dataBase from '../firebase';
+
+const useNotes = () => {
+  const [defaultNotes, setDefaultNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [newNote, setNewNote] = useState<Note>(defaultNote);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [editedNote, setEditedNote] = useState<Note>(defaultNote);
+
+  const notesCollection = collection(dataBase, 'notes');
+
+  const getNotes = async () => {
+    const data = await getDocs(notesCollection);
+    const notesFromDataBase: Note[] = data.docs.map((item) => ({
+      ...item.data(),
+      id: item.id,
+    }));
+    setNotes(notesFromDataBase);
+    setDefaultNotes(notesFromDataBase);
+  };
+
+  const createNote = async (noteToCreate: Note) => {
+    await addDoc(notesCollection, {
+      ...noteToCreate,
+      id: v4(),
+      date: new Date(),
+    });
+    await getNotes();
+    setNewNote(defaultNote);
+  };
+
+  const deleteNote = async (id: string) => {
+    const note = doc(dataBase, 'notes', id);
+    await deleteDoc(note);
+    await getNotes();
+  };
+
+  const updateNote = async (note: Note) => {
+    const updatedNote = doc(dataBase, 'notes', note.id);
+    const newNoteData = { title: note.title, text: note.text };
+    await updateDoc(updatedNote, newNoteData);
+  };
+
+  const editNote = async () => {
+    await updateNote(editedNote);
+    await getNotes();
+    setModalOpen(false);
+  };
+
+  return {
+    defaultNotes,
+    setDefaultNotes,
+    notes,
+    setNotes,
+    newNote,
+    setNewNote,
+    isModalOpen,
+    setModalOpen,
+    editedNote,
+    setEditedNote,
+    getNotes,
+    createNote,
+    deleteNote,
+    editNote,
+  };
+};
+
+export default useNotes;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,46 +6,31 @@ import Modal from 'components/Modal';
 import NoteItem from 'components/NoteItem';
 import MainPageImage from 'components/SvgElements/MainPageImage';
 
-import defaultNote from 'utils/constants';
-import { createNote, deleteNote, getNotes, updateNote } from 'utils/dataBase';
 import sortByDate from 'utils/functions';
-import Note from 'utils/interfaces';
+
+import useNotes from 'hooks/useNotes';
 
 function MainPage() {
-  const [defaultNotes, setDefaultNotes] = useState<Note[]>([]);
-  const [notes, setNotes] = useState<Note[]>([]);
-  const [newNote, setNewNote] = useState<Note>(defaultNote);
   const [isLoading, setLoading] = useState(false);
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [editedNote, setEditedNote] = useState<Note>(defaultNote);
 
-  const getNotesFromDatabase = async () => {
-    const notesFromDataBase = await getNotes();
-    setNotes(notesFromDataBase);
-    setDefaultNotes(notesFromDataBase);
-  };
-
-  const createNewNote = async () => {
-    await createNote(newNote);
-    await getNotesFromDatabase();
-    setNewNote(defaultNote);
-  };
-
-  const removeNote = async (id: string) => {
-    await deleteNote(id);
-    await getNotesFromDatabase();
-  };
-
-  const editNote = async () => {
-    await updateNote(editedNote);
-    await getNotesFromDatabase();
-    setModalOpen(false);
-  };
+  const {
+    notes,
+    newNote,
+    setNewNote,
+    isModalOpen,
+    setModalOpen,
+    editedNote,
+    setEditedNote,
+    getNotes,
+    createNote,
+    deleteNote,
+    editNote,
+  } = useNotes();
 
   useEffect(() => {
     (async () => {
       setLoading(true);
-      await getNotesFromDatabase();
+      await getNotes();
       setLoading(false);
     })();
   }, []);
@@ -59,7 +44,7 @@ function MainPage() {
             buttonText="Create"
             note={newNote}
             setNote={setNewNote}
-            onSubmit={createNewNote}
+            onSubmit={() => createNote(newNote)}
           />
         </div>
         <div className="notes-creation__image">
@@ -76,7 +61,7 @@ function MainPage() {
               key={note.id}
               title={note.title}
               text={note.text}
-              deleteNote={() => removeNote(note.id)}
+              deleteNote={() => deleteNote(note.id)}
               editNote={() => {
                 setEditedNote(note);
                 setModalOpen(true);
